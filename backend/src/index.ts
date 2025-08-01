d43e81a5f0b7091abbc533e3e778fd31686ae9ed
Mayank Sharma <102649566+MayankS1h3@users.noreply.github.com>
2025-08-01 21:59:32 +0530

    solved @types problem

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello mayank");
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import compression from "compression";
+
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.listen(5001, () => {
+  console.log(`Server is running on port 5001}`);
+});
